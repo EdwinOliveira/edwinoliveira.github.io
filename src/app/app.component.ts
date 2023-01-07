f67f917cda9a844624bc57dc824a19a575b35531
Diogo Oliveira <diogo.oliveira@bjss.com>
2023-01-07 23:20:31 +0000

    Change Modular into Standalone Application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
+  standalone: true,
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: '',
 })
-export class AppComponent {
-  title = 'my-portfolio';
-}
+export class AppComponent {}
