1999bc1194e8728fa4f36efa4ddbb3df92513c90
Diogo Oliveira <diogo.oliveira@bjss.com>
2023-01-08 15:56:15 +0000

    Worked on components

--- src/app/components/organisms/navbar/navbar.component.ts
+++ src/app/components/organisms/navbar/navbar.component.ts
@@ -1,22 +1,37 @@
 import { Component } from '@angular/core';
-import { BrandTypographyComponent } from "../../atoms/typographies/brand-typography.component";
+import { BrandTypographyComponent } from '../../atoms/typographies/brand-typography.component';
 
 @Component({
-    standalone: true,
-    selector: 'app-navbar',
-    template: `
-        <div class="wrapper">
-            <div class="wrapper__box">
-                <div class="wrapper__box icon--group"></div>
-                <div class="wrapper__box typography">
-                    <app-brand-typography [content]="'DOliveira'"></app-brand-typography>
-                </div>
-            </div>
-            <div class="wrapper__box">
-                <div class="wrapper__box icon--group"></div>
-            </div>
+  standalone: true,
+  selector: 'app-navbar',
+  template: `
+    <div class="wrapper">
+      <div class="wrapper__box">
+        <div class="wrapper__box icon--group"></div>
+        <div class="wrapper__box typography">
+          <app-brand-typography [content]="'DOliveira'"></app-brand-typography>
         </div>
+      </div>
+      <div class="wrapper__box">
+        <div class="wrapper__box icon--group"></div>
+      </div>
+    </div>
+  `,
+  styles: [
+    `
+      .wrapper {
+        height: 4rem;
+
+        &__box {
+          height: inherit;
+          display: flex;
+          flex-direction: row;
+          align-items: center;
+          justify-content: space-between;
+        }
+      }
     `,
-    imports: [BrandTypographyComponent]
+  ],
+  imports: [BrandTypographyComponent],
 })
 export class NavbarComponent {}
